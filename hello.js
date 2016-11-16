b7bf34a96e3b270c349e891b7b76d6baee3813c2
Mostafa <m.elsheikh11791@gmail.com>
2016-11-16 16:49:39 +0200

    interview code

--- hello.js
+++ hello.js
@@ -1,1 +1,21 @@
-console.log("Hello World !");
+function mathCreator() {
+    var math = {
+        add: function(x, y) {
+            return x + y;
+        }
+    }
+    return math;
+}
+
+var math = mathCreator();
+console.log(math.add(10, 20));
+
+function arraySum(array) {
+    var sum = 0;
+    for (var i = 0 ; i < array.length ; i++) {
+        sum += array[i];
+    }
+    return sum;
+}
+
+console.log(arraySum([1, 2, 3]));
